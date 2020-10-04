1fbabcf685acbcdbedd905a63272f1b57afa1d50
hashaneranda <hashaneranda@gmail.com>
2020-10-04 14:20:39 +0530

    code reafctoring

--- src/services/__test__/post.test.js
+++ src/services/__test__/post.test.js
@@ -6,8 +6,6 @@ jest.mock("axios");
 import { fetchPosts } from "../posts";
 
 describe("fetch Post Data", () => {
-  // afterEach(() => jest.resetAllMocks());
-
   it("fetches posts successfully data from an API", async () => {
     axios.get.mockImplementationOnce(() =>
       Promise.resolve({
